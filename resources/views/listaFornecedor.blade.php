356391a3b4a2c89deb18edd02bad2ac2f739e2cf
Arthur Melecardi Soares Leite <arthurmelecardi@gmail.com>
2025-06-07 19:01:16 -0300

    Criei o modal de confirmação da função deletar para um produto específico que o usuário selecionar e atualizei o hover do botão para cancelar.

--- resources/views/listaFornecedor.blade.php
+++ resources/views/listaFornecedor.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <title>Cadastro de Fornecedor</title>
+    <title>Listar Fornecedor</title>
     
     <link rel="stylesheet" href="{{ asset('css/cadastro.css') }}">
     <link rel="stylesheet" href="{{ asset('css/table.css') }}">
@@ -53,7 +53,7 @@
       </div>
     </div>
 
-    @include('modalDelete')
+    @include('modalDeleteFornecedor')
 
     @if(session('success'))
       <script>
--- resources/views/listaProduto.blade.php
+++ resources/views/listaProduto.blade.php
@@ -7,6 +7,7 @@
     
     <link rel="stylesheet" href="{{ asset('css/cadastro.css') }}">
     <link rel="stylesheet" href="{{ asset('css/table.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/modal.css') }}">
     <script src="{{ asset('js/format.js') }}"></script>
   </head>
   <body>
@@ -33,18 +34,25 @@
               <td><a href="{{ route('product.edit',$produtos) }}"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil-fill" viewBox="0 0 16 16">
                 <path d="M12.854.146a.5.5 0 0 0-.707 0L10.5 1.793 14.207 5.5l1.647-1.646a.5.5 0 0 0 0-.708zm.646 6.061L9.793 2.5 3.293 9H3.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.207zm-7.468 7.468A.5.5 0 0 1 6 13.5V13h-.5a.5.5 0 0 1-.5-.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.5-.5V10h-.5a.5.5 0 0 1-.175-.032l-.179.178a.5.5 0 0 0-.11.168l-2 5a.5.5 0 0 0 .65.65l5-2a.5.5 0 0 0 .168-.11z"/>
               </svg></a></td>
-              <td class="botao">
-                <form method="POST" action="{{ route('product.destroy',$produtos) }}">
-                  @csrf
-                  @method('DELETE')
-                  <button type="submit" class="botao-excluir">Excluir</button>
-                </form>
-              </td>
+              <td>
+                  <a href="#" onclick="showDeleteModal('{{ route('product.destroy', $produtos->id) }}')" class="botao-excluir">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash-fill" viewBox="0 0 16 16">
+                      <path d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5zM8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5zm3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0z"/>
+                    </svg>
+                  </a>
+                </td>
             </tr>
           @endforeach
           </table>
         </div>
       </div>
     </div>
+    @include('modalDeleteProduto')
+
+    @if(session('success'))
+      <script>
+        alert('{{ session('success') }}');
+      </script>
+    @endif
   </body>
 </html>
